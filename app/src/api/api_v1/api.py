0242136df7b55b30c19fba7b89c6453325adc974
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2023-01-15 22:54:46 +0400

    refactor: structure + add initial folders

--- app/src/api/api_v1/api.py
+++ app/src/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import auth
+from app.src.api.api_v1.endpoints import auth
 
 api_router = APIRouter()
 api_router.include_router(auth.router, tags=["auth"])
---
+++ app/src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from app.src.api.api_v1.api import api_router
+from app.src.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.PROJECT_VERSION,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
