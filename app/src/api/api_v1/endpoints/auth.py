c517b09f444b8491705c96e24b4ed94c86375a47
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2023-01-25 23:00:51 +0400

    feat: register endpoint

--- app/src/api/api_v1/endpoints/auth.py
+++ app/src/api/api_v1/endpoints/auth.py
@@ -1,28 +1,75 @@
-from fastapi import APIRouter, Depends, HTTPException, Request, Response, status
+from fastapi import APIRouter, HTTPException, Request, Depends, status
+from starlette_context import context
+from aioredis import Redis
+
+from src import crud
+from src.schemas.token import Token
+from src.utils.token import add_refresh_token_to_redis
+from src.schemas.auth import RegisterPayload
+from src.schemas.response import IPostResponseBase, create_response
+from src.core.security import create_access_token, create_refresh_token
+from src.db.redis import get_redis_client
 
 
 router = APIRouter()
 
+# TODO: Reverse proxy header setup (ip, user-agent, etc)
+
 
 @router.post('/login')
-async def login(request: Request, response: Response):
+async def login(request: Request):
     """Login user and create JWT access and refresh tokens"""
     pass
 
 
 @router.post('/register')
-async def register(request: Request, response: Response):
+async def register(
+    request: RegisterPayload,
+    redis_client: Redis = Depends(get_redis_client),
+) -> IPostResponseBase[Token]:
     """Register user and create JWT access and refresh tokens"""
-    pass
+
+    # Get and check required headers
+    # forwarded_for = context.data.get('X-Forwarded-For')   TODO: Figure out why this returns None
+    forwarded_for = '000.000.00.00'
+    user_agent = context.data.get('User-Agent')
+
+    if not forwarded_for:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Missing X-Forwarded-For header')
+    elif not user_agent:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Missing User-Agent header')
+
+    # Validate username and email are unique
+    user_exists = await crud.user.email_or_username_exists(email=request.email, username=request.username)
+    if user_exists:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Username or email already exists')
+
+    # Create user
+    user = await crud.user.create(obj_in=request)
+
+    # Create tokens
+    access_token = create_access_token(user.username)
+    refresh_token = create_refresh_token(user.username)
+
+    # Store refresh token and other metadata in redis
+    await add_refresh_token_to_redis(redis_client, user, refresh_token, forwarded_for, user_agent, request.fingerprint)
+
+    data = Token(
+        access_token=access_token,
+        refresh_token=refresh_token,
+        token_type="bearer"
+    )
+
+    return create_response(data=data, message="User registered successfully")
 
 
 @router.post('/logout')
-async def logout(request: Request, response: Response):
+async def logout(request: Request):
     """Logout user and revoke JWT access and refresh tokens"""
     pass
 
 
 @router.post('/refresh')
-async def refresh(request: Request, response: Response):
+async def refresh(request: Request):
     """Refresh JWT access and refresh tokens"""
     pass
---
+++ app/src/utils/token.py
@@ -0,0 +1,56 @@
+import uuid
+from typing import Optional
+
+from aioredis import Redis
+
+from src.utils.jwt import decode_jwt_token
+from src.models.user import User
+
+
+async def add_refresh_token_to_redis(
+    redis_client: Redis,
+    user: User,
+    refresh_token: str,
+    ip: str,
+    user_agent: str,
+    fingerprint: str
+) -> None:
+    """
+    Add refresh token to redis with other session information
+
+    :param user: User model instance
+    :param refresh_token: JWT token
+    :param ip: IP address
+    :param user_agent: User agent
+    :param fingerprint: Fingerprint
+    :param redis_client: Redis client
+    :return: None
+    """
+    decoded_token = decode_jwt_token(refresh_token)
+
+    # Generate a unique session ID
+    session_id = uuid.uuid4().hex
+
+    mapping = {
+        'session_id': session_id,
+        "refresh_token": refresh_token,
+        "ip": ip,
+        "user_agent": user_agent,
+        "fingerprint": fingerprint,
+        "exp": decoded_token["exp"],
+        "created_at": decoded_token["iat"],
+    }
+
+    await redis_client.sadd(f"user:{user.id}", session_id)
+    await redis_client.hset(f"user:{user.id}:{session_id}", mapping=mapping)
+
+
+async def delete_token(redis_client: Redis, user: User):
+    """
+    Delete refresh token from redis.
+
+    :param redis_client:
+    :param user:
+    :return:
+    """
+    pass
